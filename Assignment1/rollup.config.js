8d148d2f3634d86ebad1e0bb1b799490e6ee5349
2017030073 <2017030073@hanyang.ac.kr>
2020-09-26 00:01:05 +0900

    Assignment1

---
+++ Assignment1/rollup.config.js
@@ -0,0 +1,82 @@
+//만든 프로젝트를 컴파일 할 때 도움을 준다.
+//그것에 대한 설정값을 적어놓음
+//직접 건드릴 일은 없으니 가만히 냅두자
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+  let server;
+
+  function toExit() {
+    if (server) server.kill(0);
+  }
+
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require("child_process").spawn(
+        "npm",
+        ["run", "start", "--", "--dev"],
+        {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true,
+        }
+      );
+
+      process.on("SIGTERM", toExit);
+      process.on("exit", toExit);
+    },
+  };
+}
+
+export default {
+  input: "src/main.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js",
+  },
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file - better for performance
+      css: (css) => {
+        css.write("bundle.css");
+      },
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
+    commonjs(),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
+};
---
+++ Assignment1/src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+  let text = "";
+  let number = 30;
+
+  function change_font() {
+    document.getElementById("num1").style.fontSize = number;
+    document.getElementById("num2").style.fontSize = number;
+  }
+</script>
+
+<style>
+  h2 {
+    position: relative;
+    right: 5px;
+  }
+  input {
+    left: 5px;
+    position: relative;
+    float: inline-end;
+    margin-right: 30px;
+  }
+  section {
+    display: inline-flex;
+    position: relative;
+  }
+  div {
+    width: 250px;
+    height: 250px;
+    border: 1px solid black;
+    margin: 10px;
+  }
+  .par {
+    position: relative;
+    font-size: 30px;
+    top: -30px;
+    left: 10px;
+  }
+  .lato {
+    font-family: "Lato", sans-serif;
+  }
+  .noto {
+    font-family: "Noto Sans KR", sans-serif;
+  }
+</style>
+
+<!-- 
+  밑에 h2부분에서 왜 change_font가 실행이 안되는지 궁금합니다.
+  그리고 왜 change_font()라고 ()를 붙이면 에러가 발생해서
+  페이지가 뜨지 않는지 모르겠습니다..
+-->
+<html lang="kr">
+  <head>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato&family=Noto+Sans+KR:wght@700&display=swap"
+      rel="stylesheet" />
+  </head>
+  <h1>1주차 스터디 과제</h1>
+  <h2>
+    <!-- 이부분입니다. change_font()라고 ()를 붙이면 에러가 나고 안붙여도 실행은 안됩니다..-->
+    Sentence <input type="text" bind:value={text} />Font-Size <input type="number" onchange={change_font} bind:value={number} />
+  </h2>
+
+  <section>
+    <div>
+      <p id="num1" class="lato par">Lato <br /> {text}</p>
+    </div>
+    <div>
+      <p id="num2" class="noto par">noto <br />{text}</p>
+    </div>
+  </section>
+</html>
